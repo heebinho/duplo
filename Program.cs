ba44a4d105ff383c14d304f54b186d7ee5497fc2
heebinho <renatoheeb@gmail.com>
2020-04-13 13:12:03 +0200

    Empty dotnet cosole project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
